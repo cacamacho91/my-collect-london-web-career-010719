206daf76a4c0b725b85493ea4227bcd567b885bd
dashing-frame-3501 <cacamacho91@gmail.com>
2018-11-26 20:57:35 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
+def my_collect(array)
+  edited_array = []
+  iterator = 0
 
-
+  while iterator < array.length
+    edited_array << yield(array[iterator])
+    iterator += 1
+  end
+  edited_array
+end
